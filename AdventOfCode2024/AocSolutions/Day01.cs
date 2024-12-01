d45547fe7620c4ed7e3dd229b8bb275c3976bc03
Craig O'Daniel <craig.odaniel@gmail.com>
2024-12-01 12:28:51 -0500

    Add project files.

---
+++ AdventOfCode2024/AocSolutions/Day01.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    public class Day01
+    {
+        public static string fileDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\AocInputs";
+        public static string fileName = "day01.txt";
+        public static string[] lines = File.ReadAllLines(fileDir + "\\" + fileName);
+        public static void Part1()
+        {
+            Console.WriteLine("Part1");
+
+            int answer = 0;
+            int[] firstArray = GetNumberList(lines,0);
+            int[] secondArray = GetNumberList(lines,3);
+
+            Array.Sort(firstArray);
+            Array.Sort(secondArray);
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                answer += Math.Abs(secondArray[i] - firstArray[i]);
+            }
+            
+            Console.WriteLine(answer);
+            
+        }
+
+        public static void Part2() 
+        {
+            Console.WriteLine("Part2");
+            int answer = 0;
+            int[] firstArray = GetNumberList(lines, 0);
+            int[] secondArray = GetNumberList(lines, 3);
+
+            Array.Sort(firstArray);
+            Array.Sort(secondArray);
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                answer += firstArray[i] * CountMatches(firstArray[i], secondArray);
+            }
+
+            Console.WriteLine(answer);
+
+        }
+
+
+        private static int[] GetNumberList(string[] lines, int position)
+        {
+            int[] numberPairs = new int[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] line = lines[i].Split(" ");
+
+
+                numberPairs[i] = ParseStringToInt(line[position]);
+            }
+
+            return numberPairs;
+        }
+
+
+        private static int ParseStringToInt(string input)
+        {
+            try
+            {
+                int result = int.Parse(input);
+                return result;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+            
+        }
+
+        private static int CountMatches(int input, int[] secondArray)
+        {
+            int cnt = 0;
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                if (input == secondArray[i])
+                {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,68 @@
+﻿//using AocLibrary;
+
+using AdventOfCode2024;
+
+namespace AocProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("--------ADVENT OF CODE 2024--------");
+            Console.WriteLine("-----------------------------------");
+
+            while (!endApp)
+            {
+                // Declare variables.
+                string? numInput1;
+                string? numInput2;
+                string? quitInput;
+                double day;
+
+                // Ask the user to type the first number.
+                Console.Write("Type DAY # and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                while (!double.TryParse(numInput1, out day))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number.
+                Console.Write("For PART ONE enter '1', for PART TWO enter '2': ");
+                numInput2 = Console.ReadLine();
+
+                double part;
+                while (!double.TryParse(numInput2, out part))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                switch (day)
+                {
+                    case 1:
+                        if (part == 1) { Day01.Part1(); }
+                        else if (part == 2) { Day01.Part2(); }
+                        break;
+
+
+                    default:
+                        break;
+                }
+
+
+                // Ask the user if they want to end program
+                Console.Write("Quit? Y/N: ");
+                quitInput = Console.ReadLine();
+                if (quitInput == "Y" || quitInput == "y") { endApp = true; }
+
+            }
+
+        }
+    }
+}
